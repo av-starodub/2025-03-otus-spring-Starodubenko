cc7c8dd1981c47d4e5bd3527f378a89ca4c2a687
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-08-14 17:10:49 +0300

    Implement executeTest() method in TestServiceImpl class and TestServiceImplTest class

--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,30 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
+    private final QuestionDao dao;
+
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+        var questions = dao.findAll();
+        questions.forEach(question -> ioService.printFormattedLine(question.text(), getListAnswers(question)));
+    }
+
+    private List<String> getListAnswers(Question question) {
+        return question.answers().stream()
+                .map(Answer::text)
+                .toList();
     }
 }
---
+++ hw01-xml-config/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,41 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("TestServiceImplTest")
+public class TestServiceImplTest {
+    private static final List<Question> QUESTIONS = List.of(
+            new Question("q1", List.of(new Answer("A", false), new Answer("B", true)))
+    );
+
+    @Mock
+    private QuestionDao questionDao;
+
+    @Mock
+    private IOService ioService;
+
+    @InjectMocks
+    private TestServiceImpl testService;
+
+    @Test
+    @DisplayName("Should get questions from QuestionDao and pass them to IOService")
+    void executeTest() {
+        when(questionDao.findAll()).thenReturn(QUESTIONS);
+        testService.executeTest();
+        verify(ioService).printFormattedLine("q1", List.of("A", "B"));
+    }
+}
