70c95813b528e2f964134e17778c1232c5181321
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-08-21 02:14:15 +0300

    Implement testing logic in the executeTestFor() method

--- hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -6,9 +6,16 @@ import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @RequiredArgsConstructor
 @Service
 public class TestServiceImpl implements TestService {
+    public static final String QUESTION_FORMAT_TEMPLATE = "Question %d. %s";
+
+    public static final String ANSWER_FORMAT_TEMPLATE = "%d %s";
 
     private final IOService ioService;
 
@@ -20,11 +27,39 @@ public class TestServiceImpl implements TestService {
         ioService.printFormattedLine("Please answer the questions below%n");
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
-
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+        var questionsCount = 1;
+        for (var question : questions) {
+            ioService.printFormattedLine(QUESTION_FORMAT_TEMPLATE, questionsCount++, question.text());
+            var requiredCorrectAnswers = new HashSet<String>();
+            var answersCount = 1;
+            for (var answer : question.answers()) {
+                if (answer.isCorrect()) {
+                    requiredCorrectAnswers.add(String.valueOf(answersCount));
+                }
+                ioService.printFormattedLine(ANSWER_FORMAT_TEMPLATE, answersCount++, answer.text());
+            }
+            var requiredCorrectAnswersCount = requiredCorrectAnswers.size();
+            var studentAnswer = getStudentAnswer(requiredCorrectAnswersCount);
+            boolean isStudentAnswerValid = validateStudentAnswer(requiredCorrectAnswers, studentAnswer);
+            testResult.applyAnswer(question, isStudentAnswerValid);
         }
         return testResult;
+    }
+
+    private String getStudentAnswer(int requiredCorrectAnswersCount) {
+        var studentAnswer = "";
+        if (requiredCorrectAnswersCount > 1) {
+            studentAnswer = ioService.readStringWithPrompt("Write answers down separated by a space and press enter");
+        } else {
+            studentAnswer = ioService.readStringWithPrompt("Write answer and press enter");
+        }
+        return studentAnswer;
+    }
+
+    private boolean validateStudentAnswer(Set<String> requiredCorrectAnswers, String studentAnswer) {
+        var studentCorrectAnswersCount = Arrays.stream(studentAnswer.split(" "))
+                .filter(requiredCorrectAnswers::contains)
+                .count();
+        return studentCorrectAnswersCount == requiredCorrectAnswers.size();
     }
 }
