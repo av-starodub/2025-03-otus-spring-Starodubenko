20a66a94ed6d3edccca2a79ebfeb3d6c293fdaa4
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-09-03 23:53:53 +0300

    Fix by code review: move @Configuration and @PropertySource annotations from AppProperties to Application

--- hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,13 +1,11 @@
 package ru.otus.hw.config;
 
 import lombok.Data;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Data
-@Configuration
-@PropertySource("classpath:application.properties")
+@Component
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     @Value("${test.RequiredPercentRightAnswersToPass}")
